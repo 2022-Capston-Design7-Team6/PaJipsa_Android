ced19beeb5b590a2e34811b617978d48fe69f646
gaeun23 <0423g@naver.com>
2022-11-02 00:15:20 +0900

    [feat] navigation 초기 세팅

---
+++ app/src/main/java/com/capstone/patech_android/ui/home/HomeFragment.kt
@@ -0,0 +1,9 @@
+package com.capstone.patech_android.ui.home
+
+import com.capstone.patech_android.R
+import com.capstone.patech_android.base.BindingFragment
+import com.capstone.patech_android.databinding.FragmentHomeBinding
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(
+    R.layout.fragment_home
+)
