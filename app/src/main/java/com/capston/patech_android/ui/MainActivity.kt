1f48faae901d9175f7a8a43b2b39e2b3e4c76579
gaeun23 <0423g@naver.com>
2022-11-01 23:18:37 +0900

    [feat] MainActivity 생성

---
+++ app/src/main/java/com/capston/patech_android/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.capston.patech_android.ui
+
+import android.os.Bundle
+import com.capston.patech_android.R
+import com.capston.patech_android.base.BindingActivity
+import com.capston.patech_android.databinding.ActivityMainBinding
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
