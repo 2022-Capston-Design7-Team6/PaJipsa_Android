aaad5490bddd88bfedc638a3c5091cbe87380e8f
gaeun23 <0423g@naver.com>
2022-11-15 01:40:56 +0900

    [feat] photo mode dialog 기능 추가

--- app/src/main/java/com/capstone/patech_android/ui/dialog/PhotoModeDialog.kt
+++ app/src/main/java/com/capstone/patech_android/ui/dialog/PhotoModeDialog.kt
@@ -1,14 +1,20 @@
 package com.capstone.patech_android.ui.dialog
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.capstone.patech_android.R
 import com.capstone.patech_android.databinding.DialogPhotoModeBinding
+import com.capstone.patech_android.util.navigate
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class PhotoModeDialog : BottomSheetDialogFragment() {
     lateinit var binding: DialogPhotoModeBinding
+
+    private val args: PhotoModeDialogArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +23,28 @@ class PhotoModeDialog : BottomSheetDialogFragment() {
     ): View {
         binding = DialogPhotoModeBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
+        openCamera()
+        openGallery()
         return binding.root
+    }
+
+    private fun openCamera() {
+        binding.btnCamera.setOnClickListener {
+            when (args.fromView) {
+                DEFAULT_CAMERA -> Log.d("defaultCamera", "open")
+                RECORD_CAMERA -> navigate(R.id.action_photoModeDialog_to_recordCameraFragment)
+            }
+        }
+    }
+
+    private fun openGallery() {
+        binding.btnGallery.setOnClickListener {
+
+        }
+    }
+
+    companion object {
+        const val DEFAULT_CAMERA = 0
+        const val RECORD_CAMERA = 1
     }
 }
