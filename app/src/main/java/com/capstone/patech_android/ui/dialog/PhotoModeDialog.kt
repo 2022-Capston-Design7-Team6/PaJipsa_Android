67d3f631edd4561dbfa3971a8d85bad51e56ccf2
gaeun23 <0423g@naver.com>
2022-11-23 23:51:36 +0900

    [feat] 수확하기/일지작성 갤러리에서 사진 가져오기

--- app/src/main/java/com/capstone/patech_android/ui/dialog/PhotoModeDialog.kt
+++ app/src/main/java/com/capstone/patech_android/ui/dialog/PhotoModeDialog.kt
@@ -1,11 +1,21 @@
 package com.capstone.patech_android.ui.dialog
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.capstone.patech_android.databinding.DialogPhotoModeBinding
+import com.capstone.patech_android.ui.harvest.aftercamera.AfterCameraFragment
+import com.capstone.patech_android.ui.harvest.beforecamera.BeforeCameraFragment
+import com.capstone.patech_android.ui.record.RecordCameraFragment
 import com.capstone.patech_android.util.navigateWithData
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -51,9 +61,42 @@ class PhotoModeDialog : BottomSheetDialogFragment() {
         }
     }
 
+    @SuppressLint("IntentReset")
     private fun openGallery() {
+        val requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+                if (isGranted) {
+                    val intent = Intent(Intent.ACTION_PICK)
+                    intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    intent.type = "image/*"
+                    getImage.launch(intent)
+                }
+            }
         binding.btnGallery.setOnClickListener {
+            requestPermissionLauncher.launch(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        }
+    }
 
+    private val getImage = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result: ActivityResult ->
+        if (result.data != null) {
+            val imageUri = result.data?.data
+            findNavController().previousBackStackEntry?.savedStateHandle?.set(
+                setTag(),
+                imageUri
+            )
+            dismiss()
+        }
+    }
+
+    private fun setTag(): String {
+        return when (args.fromView) {
+            RECORD_CAMERA -> RecordCameraFragment.RECORD_URI
+            HARVEST_BEFORE_CAMERA -> BeforeCameraFragment.BEFORE_URI
+            else -> AfterCameraFragment.AFTER_URI
         }
     }
 
