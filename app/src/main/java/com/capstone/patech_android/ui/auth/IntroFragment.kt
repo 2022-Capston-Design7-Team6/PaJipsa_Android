50b184e30230191c3c99a2a85d8885ad7dc8650b
gaeun23 <0423g@naver.com>
2022-11-30 22:52:18 +0900

    [feat] splash 구현, 자동로그인 구현

--- app/src/main/java/com/capstone/patech_android/ui/auth/IntroFragment.kt
+++ app/src/main/java/com/capstone/patech_android/ui/auth/IntroFragment.kt
@@ -1,10 +1,15 @@
 package com.capstone.patech_android.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import com.capstone.patech_android.R
 import com.capstone.patech_android.base.BindingFragment
+import com.capstone.patech_android.data.SharedPreferenceController
 import com.capstone.patech_android.databinding.FragmentAuthIntroBinding
+import com.capstone.patech_android.ui.MainActivity
 import com.capstone.patech_android.util.navigate
 
 class IntroFragment : BindingFragment<FragmentAuthIntroBinding>(
@@ -13,6 +18,22 @@ class IntroFragment : BindingFragment<FragmentAuthIntroBinding>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         addListener()
+        checkAutoLogin()
+    }
+
+    private fun checkAutoLogin() {
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val token = SharedPreferenceController.getToken()
+                if (token != null && token.isNotBlank()) {
+                    startActivity(Intent(requireContext(), MainActivity::class.java))
+                    requireActivity().finish()
+                } else {
+                    binding.splash.visibility = View.GONE
+                    binding.layoutIntro.visibility = View.VISIBLE
+                }
+            }, 1500
+        )
     }
 
     private fun addListener() {
